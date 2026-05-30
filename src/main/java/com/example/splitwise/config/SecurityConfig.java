73799657ed6f6398cb3e1c89362be6cedcb0fab8
Diya Dengre <dengrediya@gmail.com>
2026-05-30 20:34:15 +0530

    Fix CORS

--- src/main/java/com/example/splitwise/config/SecurityConfig.java
+++ src/main/java/com/example/splitwise/config/SecurityConfig.java
@@ -2,26 +2,21 @@ package com.example.splitwise.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(
-            HttpSecurity http
-    ) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-
                 .csrf(csrf -> csrf.disable())
-
                 .cors(cors -> {})
-
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().permitAll()
                 );
 
