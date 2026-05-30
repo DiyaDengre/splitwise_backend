4399adce70f7c109a3f650286de62593dc1945ca
Diya Dengre <dengrediya@gmail.com>
2026-05-30 20:25:56 +0530

    Enable CORS

--- src/main/java/com/example/splitwise/config/CorsConfig.java
+++ src/main/java/com/example/splitwise/config/CorsConfig.java
@@ -1,4 +1,5 @@
 package com.example.splitwise.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,19 +10,11 @@ public class CorsConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-
         return new WebMvcConfigurer() {
-
             @Override
-            public void addCorsMappings(
-                    CorsRegistry registry
-            ) {
-
+            public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:5173",
-                                "http://localhost:5174"
-                        )
+                        .allowedOrigins("https://splitwise-swart.vercel.app")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
