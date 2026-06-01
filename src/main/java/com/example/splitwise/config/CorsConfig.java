6ffa4b5945231d6dce047e96728717037b5b9373
Diya Dengre <dengrediya@gmail.com>
2026-06-01 20:42:49 +0530

    Fix CORS

--- src/main/java/com/example/splitwise/config/CorsConfig.java
+++ src/main/java/com/example/splitwise/config/CorsConfig.java
@@ -11,12 +11,14 @@ public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
-
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-
                 registry.addMapping("/**")
-                        .allowedOrigins("https://splitwise-swart.vercel.app")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "http://localhost:5174",
+                                "https://splitwise-swart.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
